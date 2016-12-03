156b45ab8bfb446b00b5798d526776b48cfba4a7
xNehel <eskrzy@gmail.com>
2016-12-03 16:26:59 +0100

    + Added click state on expansion filter

--- src/pages/cards/filters/expansions.js
+++ src/pages/cards/filters/expansions.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 import {expansions} from '../data.filters';
 
 export class ExpansionFilter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {active: null};
+  }
+  handleClick(i) {
+    let isActive = this.state.active === i ? null : i;
+    this.setState({active: isActive});
+  }
   render() {
     return (
         <div>
           <h3>Dodatki</h3>
           <ul className="">
             {expansions.map((element, index) =>
-                <li className={element} value={element} key={index}>
+                <li onClick={this.handleClick.bind(this, index)} className={`${element} ${this.state.active === index && 'icon-active'}`} value={element} key={index}>
                   <span className={`hs icon-${element}`}></span>
                 </li>
             )}
