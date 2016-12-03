0a3aef20a5cc7fa220a91c81be42aac059652c7f
xNehel <eskrzy@gmail.com>
2016-12-03 16:19:12 +0100

    + Added click state on class filter

--- src/pages/cards/filters/hs-class.js
+++ src/pages/cards/filters/hs-class.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react';
 import {hs_class} from '../data.filters';
 
 export class HsClassFilter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {active: null};
+  }
+  handleClick(i) {
+    let isActive = this.state.active === i ? null : i;
+    this.setState({active: isActive});
+  }
+
   render() {
     return (
         <ul className="topbar-right">
           {hs_class.map((element, index) =>
-              <li className={element} value={element} key={index}>
+              <li onClick={this.handleClick.bind(this, index)} className={`${element} ${this.state.active === index && 'icon-active'}`} value={element} key={index}>
                 <span className={`hs icon-${element}`}></span>
               </li>
           )}
