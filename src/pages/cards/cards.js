a5b86e681cc4dd4c76637660c7d92bb2997ddeaf
xNehel <eskrzy@gmail.com>
2016-12-09 14:03:59 +0100

    +Added service file in cards for future api requests ~ Added inner containers for easier styling in cards

--- src/pages/cards/cards.js
+++ src/pages/cards/cards.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import 'react-select/dist/react-select.css';
 import {Sidebar} from './sidebar';
 import {Topbar} from './topbar';
+import {ServiceCards} from './service.cards';
 
 export class Cards extends Component {
   render() {
     return (
         <div className="pageContainer cards">
-          <Sidebar/>
-          <Topbar/>
+            <div className="left-container">
+                <Sidebar/>
+            </div>
+            <div className="right-container">
+                <Topbar/>
+                <ServiceCards/>
+            </div>
         </div>
     );
   }
---
+++ src/pages/cards/service.cards.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+export class ServiceCards extends Component {
+    render() {
+        return (
+            <div className="cards-container">
+                <p className="card">test</p>
+                <p className="card">test</p>
+                <p className="card">test</p>
+                <p className="card">test</p>
+                <p className="card">test</p>
+                <p className="card">test</p>
+                <p className="card">test</p>
+                <p className="card">test</p>
+                <p className="card">test</p>
+                <p className="card">test</p>
+                <p className="card">test</p>
+                <p className="card">test</p>
+                <p className="card">test</p>
+                <p className="card">test</p>
+                <p className="card">test</p>
+                <p className="card">test</p>
+                <p className="card">test</p>
+                <p className="card">test</p>
+            </div>
+        );
+    }
+}
