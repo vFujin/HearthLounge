7aaf0a344b16332b271e428c3e4aa60c3c44f399
xNehel <eskrzy@gmail.com>
2016-12-30 18:59:46 +0100

    Minor styling changes

--- src/pages/home/blocks/decks/decks.js
+++ src/pages/home/blocks/decks/decks.js
@@ -3,26 +3,34 @@ import { Link } from 'react-router'
 import { ClassSelection } from './class-selection';
 import { ModeSelection } from './mode-selection';
 import { DeckSnippet } from './deck-snippet/deck-snippet';
+import { SearchDecks } from './search-decks';
 export class DecksBlock extends Component {
   render() {
     return (
       <li className={'home__block decks block-width-3'}>
-        <div className="topbar">
-          <ClassSelection />
-        </div>
-        <div className="sidebar">
-          <ModeSelection />
-        </div>
-        <div className="hot-decks">
-          <div className="upper-row">
-            <DeckSnippet/>
-            <DeckSnippet/>
-            <DeckSnippet/>
+
+        <div className="left-container">
+          <div className="sidebar">
+            <SearchDecks/>
+            <ModeSelection />
           </div>
-          <div className="bottom-row">
-            <DeckSnippet/>
-            <DeckSnippet/>
-            <DeckSnippet/>
+        </div>
+
+        <div className="right-container">
+          <div className="class-filter">
+            <ClassSelection/>
+          </div>
+          <div className="hot-decks">
+            <div className="upper-row">
+              <DeckSnippet/>
+              <DeckSnippet/>
+              <DeckSnippet/>
+            </div>
+            <div className="bottom-row">
+              <DeckSnippet/>
+              <DeckSnippet/>
+              <DeckSnippet/>
+            </div>
           </div>
         </div>
       </li>
---
+++ src/pages/home/blocks/decks/search-decks.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+export class SearchDecks extends Component {
+  render() {
+    return (
+        <div className="search-decks">
+          <input type="text" placeholder="Wyszukaj talie kart..." />
+        </div>
+    );
+  }
+}
