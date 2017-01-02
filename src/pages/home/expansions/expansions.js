e5fc3f1f53dc7aba18386b4a87596fa23291a3ef
xNehel <eskrzy@gmail.com>
2017-01-02 21:12:48 +0100

    Added slider view on home page

--- src/pages/home/expansions/expansions.js
+++ src/pages/home/expansions/expansions.js
@@ -6,10 +6,24 @@ export class ExpansionsBlock extends Component {
     return (
         <li className={'home__block expansions block-width-2'}>
           <Link to={'/dodatki'}>
-            <div>Dodatki</div>
+            <div className="header">Dodatki</div>
           </Link>
-          <ExpansionSlider />
-          test
+          <div className="slider">
+            <ul>
+              <li>
+                <img src="https://raw.githubusercontent.com/xNehel/clownfiesta-collector-react/master/src/images/home.expansion-slider/czarna_gora.jpg" alt="bm"/>
+              </li>
+            </ul>
+            <div className="slider-nav">
+              <div className="prev">&#10094;</div>
+              <div className="next">&#10095;</div>
+              <div className="bullets">
+                <ul>
+                  <li>&#8226;</li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </li>
     );
   }
