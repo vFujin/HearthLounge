a2bd6241e8457673311902983630e28c7914ece8
xNehel <eskrzy@gmail.com>
2017-01-23 22:26:52 +0100

    Added sidebar inputs

--- src/containers/pages/dashboard/left-container/sidebar.js
+++ src/containers/pages/dashboard/left-container/sidebar.js
@@ -6,22 +6,67 @@ export class Sidebar extends Component {
     return (
         <div className="sidebar">
           <h3 className="filter-header">Profile</h3>
+
           <ul>
-            <li className="avatar"><img src="http://lorempixel.com/200/200/cats/" alt="cat"/></li>
-            <li className="username">xx_Joe_xx</li>
-            <li className="email">joe@gmail.com</li>
-            <li className="firstname">Joe</li>
-            <li className="lastname">Smith</li>
-            <li className="gender">Male</li>
-            <li className="country">'Murica</li>
-            <li className="signature">random text</li>
-            <li className="battletag">placeholder#0000</li>
-            <li className="favourite-class">Warlock</li>
-            <li className="region">EU</li>
-            <li className="facebook">www.facebook.com/placeholder</li>
-            <li className="twitter">www.twitter.com/placeholder</li>
-            <li className="twitch">www.twitch.com/placeholder</li>
-            <li className="youtube">www.youtube.com/placeholder</li>
+            <li className="about">
+              <div className="avatar"><img src="http://lorempixel.com/100/100/cats/" alt="cat"/></div>
+              <div className="username">xx_Joe_xx</div>
+              <label htmlFor="email">
+                <input id="email" type="text" value="joe@placeholder.com"/>
+              </label>
+            </li>
+
+            <li className="details">
+              <h3><span>Details</span></h3>
+              <label htmlFor="firstname">
+                <p>First name:</p>
+                <input id="firstname" type="text" value="Joe"/>
+              </label>
+              <label htmlFor="lastname">
+                <p>First name:</p>
+                <input id="lastname" type="text" value="Smith"/>
+              </label>
+              <div className="gender">Male</div>
+              <div className="country">'Murica</div>
+              <div className="signature">random text</div>
+            </li>
+
+            <li className="hearthstone">
+              <h3><span>Hearthstone</span></h3>
+              <label htmlFor="battletag">
+                <span className="hs-icon icon-battlenet"></span>
+                <input id="battletag" type="text" value="Placeholder#0000"/>
+              </label>
+              <label htmlFor="favourite-class">
+                <p>Favourite class:</p>
+                <span className="hs-icon icon-warlock"></span>
+                <input id="favourite-class" type="text" value="Warlock"/>
+              </label>
+              <label htmlFor="region">
+                <p>Region:</p>
+                <input id="region" type="text" value="EU"/>
+              </label>
+            </li>
+
+            <li className="social-media">
+              <h3><span>Social Media</span></h3>
+              <label htmlFor="facebook">
+                <span className="hs-icon icon-facebook"></span>
+                <input id="facebook" type="text" value="Placeholder"/>
+              </label>
+              <label htmlFor="twitter">
+                <span className="hs-icon icon-twitter"></span>
+                <input id="twitter" type="text" value="Placeholder"/>
+              </label>
+              <label htmlFor="twitch">
+                <span className="hs-icon icon-twitch"></span>
+                <input id="twitch" type="text" value="Placeholder"/>
+              </label>
+              <label htmlFor="youtube">
+                <span className="hs-icon icon-youtube"></span>
+                <input id="youtube" type="text" value="Placeholder"/>
+              </label>
+            </li>
           </ul>
           <button>Edit</button>
         </div>
