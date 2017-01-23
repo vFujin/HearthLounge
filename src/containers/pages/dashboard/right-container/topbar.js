a3f9bb7d5050e87b0ba171b53b7aff0bb28c614b
xNehel <eskrzy@gmail.com>
2017-01-23 21:19:47 +0100

    Added tooltips to topbar in dashboard

--- src/containers/pages/dashboard/right-container/topbar.js
+++ src/containers/pages/dashboard/right-container/topbar.js
@@ -2,23 +2,43 @@ import React, { Component } from 'react';
 export class Topbar extends Component {
   render() {
     return (
-        <div className="topbar">
-          <ul>
-            <li className="home-block-scheme">Home page block scheme</li>
+        <ul className="topbar has-icons">
+            <li className="home-grid">
+              <span className="hs-icon icon-grid"></span>
+              <div className="tooltip">
+                <div className="caret-up"></div>
+                <p>Homepage block scheme</p>
+              </div>
+            </li>
             <li className="deck">
               <span className="hs-icon icon-deck"></span>
+              <div className="tooltip">
+                <div className="caret-up"></div>
+                <p>Decks</p>
+              </div>
             </li>
             <li className="forum">
-              <span className="hs-icon icon-forum"></span>
+              <span className="hs-icon icon-bubbles2"></span>
+              <div className="tooltip">
+                <div className="caret-up"></div>
+                <p>Forum</p>
+              </div>
             </li>
             <li className="tournaments">
               <span className="hs-icon icon-trophy"></span>
+              <div className="tooltip">
+                <div className="caret-up"></div>
+                <p>Tournaments</p>
+              </div>
             </li>
             <li className="streams">
               <span className="hs-icon icon-twitch"></span>
+              <div className="tooltip">
+                <div className="caret-up"></div>
+                <p>Streams</p>
+              </div>
             </li>
-          </ul>
-        </div>
+        </ul>
     );
   }
 }
