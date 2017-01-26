5692970a5c98283e4b7ba4d03f516ca7d7ca4d6f
xNehel <eskrzy@gmail.com>
2017-01-26 06:33:29 +0100

    Added kinda login form

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -2,11 +2,10 @@ import React, { Component } from 'react';
 import {Navbar} from './layout/navbar';
 import {Footer} from './layout/footer';
 export class Main extends Component {
-
   render() {
     return (
         <div>
-          <Navbar/>
+          <Navbar />
           {this.props.children}
           <Footer/>
         </div>
--- src/containers/layout/navbar.js
+++ src/containers/layout/navbar.js
@@ -3,13 +3,42 @@ import { Link } from 'react-router'
 import { navItems } from '../data/nav';
 
 export class Navbar extends Component {
+  constructor(props){
+    super(props);
+
+    this.state={
+      loginPopup: 'display-none'
+    }
+  }
+
+  handleNavigationLoginClick(i){
+    let isActive = i === 10 ? 'display-block' : 'display-none';
+
+    console.log(i);
+    this.setState({
+      loginPopup: isActive
+    });
+  }
+
+  handleEscKeyPress(e){
+    if(e.keyCode === 27){
+      this.setState({
+        loginPopup: 'display-none'
+      })
+    }
+  }
+
+  componentDidMount(){
+    document.addEventListener('keydown', this.handleEscKeyPress.bind(this))
+  }
+
   dropdown(el, index){
     let sub = el.submenu;
     if(!el.hasOwnProperty('submenu')) return;
     return (
         <ul className="submenu">
           {navItems[index].submenu.map( (item, id) =>
-            <li data-adventure={item.submenu_url} className={sub[id].submenu_li_className} key={id }>
+            <li data-adventure={item.submenu_url} className={sub[id].submenu_li_className} key={id}>
               <Link to={'/' + el.url + '/' + sub[id].submenu_url} activeClassName="submenu-active">
                 <span className={`icon-${sub[id].submenu_li_className}`}></span>
                 <div className="icon-label">{sub[id].submenu_li_title}</div>
@@ -25,7 +54,7 @@ export class Navbar extends Component {
           <div className="logo"></div>
           <ul>
             {navItems.map((element, index) =>
-                <li key={index} className={element.className}>
+                <li key={index} className={element.className} onClick={this.handleNavigationLoginClick.bind(this, index)}>
                   <Link to={'/' + element.url} activeClassName="active">
                     <span className={element.icon}></span>
                     <div>{element.name}</div>
@@ -34,6 +63,9 @@ export class Navbar extends Component {
                 </li>
             )}
           </ul>
+          <div className={`login-popup ${this.state.loginPopup}`}>
+              <span className="login"></span>
+          </div>
         </nav>
 
     );
