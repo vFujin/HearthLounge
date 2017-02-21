501920c7bbdf791a42c9eb6e0fa8a7ed29813893
xNehel <eskrzy@gmail.com>
2017-02-21 18:17:58 +0100

    Fixed class structure tab in adventure

--- src/containers/pages/adventures/assets/structure.js
+++ src/containers/pages/adventures/assets/structure.js
@@ -3,23 +3,23 @@ import { adventure_details } from '../../../../data/adventure-details';
 export class AdventureStructure extends Component {
   render() {
     return (
-        <div className={`structure inner-container ${this.props.active === 'structure' && 'active'}-view`}>
-          {adventure_details.map((element, index)=>
-              <div className={`${this.props.adventure === element.adventure && 'active'}-view`} key={index}>
+        <div className={`structure inner-container ${this.props.details === 'structure' && 'active'}-view`}>
+          {adventure_details.map((adventure, index)=>
+              <div className={`${this.props.adventure === adventure.adventure && 'active'}-view`} key={index}>
 
                 <ul>
-                  <li>{element.structure.wing_amount} skrzydeł</li>
+                  <li>{adventure.structure.wing_amount} wings</li>
                   <li>
                     <ul>
-                      {element.structure.wing_details.map((element,index)=>
+                      {adventure.structure.wing_details.map((element,index)=>
                         <li key={index}>{element}</li>
                       )}
                     </ul>
                   </li>
-                  <li>{element.structure.bosses_amount} bossów</li>
-                  <li>{element.structure.bosses_difficulty}</li>
-                  <li>{element.structure.class_challanges} wyzwań klasowych</li>
-                  <li>{element.structure.class_challanges_details}</li>
+                  <li>{adventure.structure.bosses_amount} bosses</li>
+                  <li>{adventure.structure.bosses_difficulty}</li>
+                  <li>{adventure.structure.class_challanges} class challanges</li>
+                  <li>{adventure.structure.class_challanges_details}</li>
                 </ul>
               </div>
           )}
--- src/containers/pages/adventures/right-container/details.js
+++ src/containers/pages/adventures/right-container/details.js
@@ -14,9 +14,9 @@ export class AdventureDetails extends Component {
         <AdventureBosses adventure={adventureUrl} details={detailsUrl}/>
         <AdventureCards adventure={adventureUrl} details={detailsUrl}/>
         <AdventureClassChallanges adventure={adventureUrl} details={detailsUrl}/>
-
         <AdventureCost adventure={adventureUrl} details={detailsUrl}/>
-        <AdventureStructure adventure={this.props.sidebarActiveTab} active={this.props.topbarActiveTabUrl}/>
+
+        <AdventureStructure adventure={adventureUrl} details={detailsUrl}/>
         {this.props.children}
       </div>
     )
