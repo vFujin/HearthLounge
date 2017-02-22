1e8adcf2737011ce1f2bc279d1eb6da37c6af1f7
xNehel <eskrzy@gmail.com>
2017-02-22 15:10:38 +0100

    Changed component listing to switch statement

--- src/containers/pages/adventures/right-container/details.js
+++ src/containers/pages/adventures/right-container/details.js
@@ -8,19 +8,25 @@ import {AdventureStructure} from '../assets/structure';
 import {AdventureBoss} from '../assets/adventure-boss';
 
 export class AdventureDetails extends Component {
+  activeDetailsContent(adventureUrl, detailsUrl, bossUrl){
+    switch(detailsUrl){
+      case 'overview':          return <AdventureOverview         adventure={adventureUrl} details={detailsUrl}/>;
+      case 'bosses':            return <AdventureBosses           adventure={adventureUrl} details={detailsUrl}/>;
+      case 'cards':             return <AdventureCards            adventure={adventureUrl} details={detailsUrl}/>;
+      case 'class-challanges':  return <AdventureClassChallanges  adventure={adventureUrl} details={detailsUrl}/>;
+      case 'cost':              return <AdventureCost             adventure={adventureUrl} details={detailsUrl}/>;
+      case 'structure':         return <AdventureStructure        adventure={adventureUrl} details={detailsUrl}/>;
+      case 'boss':              return <AdventureBoss             adventure={adventureUrl} details={detailsUrl} boss={bossUrl}/>;
+    }
+  }
   render(){
     let adventureUrl = this.props.params.adventure;
     let detailsUrl = this.props.params.details;
     let bossUrl = this.props.params.boss;
+
     return (
       <div className={`extension-content ${this.props.sidebarActiveTab} ${this.props.details} `}>
-        <AdventureOverview        adventure={adventureUrl} details={detailsUrl}/>
-        <AdventureBosses          adventure={adventureUrl} details={detailsUrl}/>
-        <AdventureCards           adventure={adventureUrl} details={detailsUrl}/>
-        <AdventureClassChallanges adventure={adventureUrl} details={detailsUrl}/>
-        <AdventureCost            adventure={adventureUrl} details={detailsUrl}/>
-        <AdventureStructure       adventure={adventureUrl} details={detailsUrl}/>
-        <AdventureBoss            adventure={adventureUrl} details={detailsUrl} boss={bossUrl}/>
+        {this.activeDetailsContent(adventureUrl, detailsUrl, bossUrl)}
       </div>
     )
   }
