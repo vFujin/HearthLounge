2263c38ec6ffff11f3f00abfdcebdfe12be9ad5f
xNehel <eskrzy@gmail.com>
2017-02-28 17:48:23 +0100

    Added sidebar/topbar layout for reddit

--- src/containers/pages/reddit/posts/posts.js
+++ src/containers/pages/reddit/posts/posts.js
@@ -1,72 +1,76 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router';
-export class RedditPosts extends Component{
+export class RedditPosts extends Component {
 
-  icon(post){
-    function iconTemplate(icon){
+  icon(post) {
+    function iconTemplate(icon) {
       return <span className={`hs hs-icon icon-${icon}`}></span>;
     }
 
-    if(post.domain == "youtu.be"){
+    if (post.domain == "youtu.be") {
       console.log(post.domain);
       return iconTemplate('youtube');
     }
-    if(post.domain.includes("battle.net")){
+    if (post.domain.includes("battle.net")) {
       return iconTemplate('battlenet');
     }
-    if(post.domain == "self.hearthstone" && post.link_flair_text.toLowerCase() != "tournament") {
+    if (post.domain == "self.hearthstone" && post.link_flair_text.toLowerCase() != "tournament") {
       return iconTemplate('bubbles2');
     }
-    if(post.domain == "self.hearthstone" && post.link_flair_text.toLowerCase() == "tournament") {
+    if (post.domain == "self.hearthstone" && post.link_flair_text.toLowerCase() == "tournament") {
       return iconTemplate('trophy');
     }
-    if(post.domain != "self.hearthstone" ){
+    if (post.domain != "self.hearthstone") {
       let icon = post.domain.replace(/.com|clips.|.tv/g, "").toLowerCase();
       return iconTemplate(icon);
     }
   }
 
 
-  stripRedditURL(url){
+  stripRedditURL(url) {
     let split = url.split('/');
-    let postUrl = split[split.length-2];
+    let postUrl = split[split.length - 2];
     return postUrl;
   }
 
-  checkDomain(post){
+  checkDomain(post) {
     let postURL = this.stripRedditURL;
     let selfURL = `/reddit/post/${post.id}/${postURL(post.permalink)}`;
 
-    switch(post.domain){
+    switch (post.domain) {
       case 'self.hearthstone':
       case 'twitter.com':
       case 'youtube.com':
       case 'youtu.be':
       case 'clips.twitch.tv':
         return selfURL;
-      default: return post.url;
+      default:
+        return post.url;
     }
   }
 
-  render(){
+  render() {
     return (
-      <table>
-        <tbody>
-        <tr>
-          <th>Upvotes</th>
-          <th>Domain</th>
-          <th>Title</th>
-        </tr>
-        {this.props.posts.map(post=>(
-                <tr key={post.id} onClick={this.props.handleRedditPostClick.bind(this, post)}>
-                  <td><Link to={this.checkDomain(post)}>{post.ups}</Link></td>
-                  <td><Link to={this.checkDomain(post)}>{this.icon(post)}</Link></td>
-                  <td><Link to={this.checkDomain(post)}>{post.title.replace('&amp;', '&').replace('&gt;', '>')}</Link></td>
-                </tr>
-            )
-        )}
-        </tbody>
+      <div className="posts">
+        <table>
+          <tbody>
+            <tr>
+              <th>Upvotes</th>
+              <th>Domain</th>
+              <th>Title</th>
+            </tr>
+          {this.props.posts.map(post => (
+              <tr key={post.id} onClick={this.props.handleRedditPostClick.bind(this, post)}>
+                <td><Link to={this.checkDomain(post)}>{post.ups}</Link></td>
+                <td><Link to={this.checkDomain(post)}>{this.icon(post)}</Link></td>
+                <td><Link to={this.checkDomain(post)}>{post.title.replace('&amp;', '&').replace('&gt;', '>')}</Link>
+                </td>
+              </tr>
+              )
+          )}
+          </tbody>
         </table>
+      </div>
     )
   }
 }
--- src/containers/shared-assets/filters/hs-class.js
+++ src/containers/shared-assets/filters/hs-class.js
@@ -10,7 +10,7 @@ const HsClassFilter = (props) => {
       hs_class.map((hs_class, index) =>
         <li key={index}>
           <Link to={`${page}?class=${hs_class.url}`}>
-            <span className={`hs-icon icon-${hs_class.url}`}></span>
+            <span className={`hs-icon ${hs_class.name} icon-${hs_class.url}`}></span>
             <div className="tooltip">
               <div className="caret-up"></div>
               <p>{hs_class.name}</p>
