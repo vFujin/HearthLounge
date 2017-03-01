faf2c8203cc29bb8942dc7b1ca9e7c1e735191e9
xNehel <eskrzy@gmail.com>
2017-03-01 19:26:15 +0100

    Added error styling

--- src/containers/pages/login/sign-up.js
+++ src/containers/pages/login/sign-up.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router';
+import FormTooltips from './form-tooltips';
+
 export class SignUp extends Component {
   constructor(props){
     super(props);
@@ -21,6 +23,10 @@ export class SignUp extends Component {
 
   compareInputValues(input, confirm_input){
 
+  }
+
+  showTooltip(){
+    return <FormTooltips />
   }
 
   handleInputChange(e){
@@ -48,35 +54,55 @@ export class SignUp extends Component {
     return (
       <div className="sign sign-up active">
         <form onSubmit={this.handleFormSubmit}>
-          <label htmlFor="username">Username:</label>
-          <input onChange={this.handleInputChange} type="text" id="username" placeholder="xJoex"/>
+          <div className="input-wrapper">
+            <label htmlFor="username">Username:</label>
+            <input onChange={this.handleInputChange} type="text" id="username" placeholder="xJoex"/>
+          </div>
 
-          <label htmlFor="email">E-mail:</label>
-          <input onChange={this.handleInputChange} type="email" id="email" placeholder="example@example.com"/>
+          <div className="input-wrapper">
+            <label htmlFor="email">E-mail:</label>
+            <input onChange={this.handleInputChange} type="email" id="email" placeholder="example@example.com"/>
+            <div className="err death-knight active">
+              <span className="caret-left"></span>
+              <p>E-mail addresses do not match! E-mail addresses do not match! E-mail addresses do not match!</p>
+            </div>
+          </div>
 
-          <label htmlFor="email_confirm">Confirm e-mail:</label>
-          <input onChange={this.handleInputChange} type="email" id="email_confirm" placeholder="example@example.com"/>
+          <div className="input-wrapper">
+            <label htmlFor="email_confirm">Confirm e-mail:</label>
+            <input onChange={this.handleInputChange} type="email" id="email_confirm" placeholder="example@example.com"/>
+          </div>
 
+          <div className="input-wrapper">
           <label htmlFor="password">Password:</label>
           <input onChange={this.handleInputChange} type="password" id="password"/>
+          </div>
 
-          <label htmlFor="password_confirm">Confirm password:</label>
-          <input onChange={this.handleInputChange} type="password" id="confirm_password"/>
+          <div className="input-wrapper">
+            <label htmlFor="password_confirm">Confirm password:</label>
+            <input onChange={this.handleInputChange} type="password" id="confirm_password"/>
+          </div>
 
-          <label htmlFor="secret_question">Choose secret question:</label>
-          <select id="secret_question">
-            <option>What was your childhood nickname?</option>
-            <option>In what city did you meet your spouse/significant other?</option>
-            <option>What is the name of your favorite childhood friend?</option>
-            <option>In what city or town did your mother and father meet?</option>
-          </select>
+          <div className="input-wrapper">
+            <label htmlFor="secret_question">Choose secret question:</label>
+            <select id="secret_question">
+              <option>What was your childhood nickname?</option>
+              <option>In what city did you meet your spouse/significant other?</option>
+              <option>What is the name of your favorite childhood friend?</option>
+              <option>In what city or town did your mother and father meet?</option>
+            </select>
+          </div>
 
-          <label htmlFor="secret_answer">Secret answer:</label>
-          <input onChange={this.handleInputChange} type="text" id="secret_answer"/>
+          <div className="input-wrapper">
+            <label htmlFor="secret_answer">Secret answer:</label>
+            <input onChange={this.handleInputChange} type="text" id="secret_answer"/>
+          </div>
 
-          <div className="tos">
-            <input onClick={this.handleCheckboxClick} id="tos" type="checkbox"/>
-            <label htmlFor="tos">I agree to the <Link to="terms-of-service">terms of service</Link></label>
+          <div className="input-wrapper">
+            <div className="tos">
+              <input onClick={this.handleCheckboxClick} id="tos" type="checkbox"/>
+              <label htmlFor="tos">I agree to the <Link to="terms-of-service">terms of service</Link></label>
+            </div>
           </div>
           {/*place for captcha*/}
 
