9ec62ef8a9f5039636f6e25d72e8ef1920eb0738
xNehel <eskrzy@gmail.com>
2017-03-03 14:05:00 +0100

    Added rest requirements

--- src/data/form-requirements.js
+++ src/data/form-requirements.js
@@ -3,7 +3,7 @@ export const requirementsObj = {
   username: ["start with letter", "be between 3-10 characters", "contain only [a-zA-Z] characters"],
   ['e-mail']: ["look like: example@example.com"],
   ['confirm_e-mail']: ["be the same as e-mail address written above"],
-  password: ["be K"],
+  password: ["be between 8-20 characters", "have at least 1 upper case letter [A-Z]", "have at least 1 numeric character [0-9]", "have at least 1 special character [! @ # $ % ^ & *]"],
   confirm_password: ["be the same as password written above"],
-  secret: ['be x']
+  secret: ["be between 3-10 characters", "contain [a-zA-Z] characters", "contain [0-9] characters"]
 };
