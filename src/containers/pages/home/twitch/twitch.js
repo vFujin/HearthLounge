afd1ea0a188536caf9dfd258b9b0139f7f1ca8a5
xNehel <eskrzy@gmail.com>
2017-03-10 16:35:09 +0100

    Added streamer state and handleStreamer func

--- src/containers/pages/home/twitch/twitch.js
+++ src/containers/pages/home/twitch/twitch.js
@@ -2,18 +2,18 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import {TwitchClientId} from "../../../../keys";
 
-
-
 export class TwitchBlock extends Component {
   constructor(props){
     super(props);
 
     this.state = {
-      streams: []
+      streams: [],
+      streamer: null
     }
   }
+
   componentDidMount() {
-    fetch('https://api.twitch.tv/kraken/search/streams?query=hearthstone&limit=7&muted=true', {
+    fetch('https://api.twitch.tv/kraken/search/streams?query=hearthstone&limit=7', {
       headers: {
         "Accept": 'application/vnd.twitchtv.v5+json',
         "Client-Id": TwitchClientId
@@ -23,18 +23,33 @@ export class TwitchBlock extends Component {
       .then(data => {
         console.log(data.streams);
         this.setState({
-          streams: data.streams
+          streams: data.streams,
+          streamer: data.streams[0].channel.name
         })
       })
+  }
+
+  handleStreamerSelectionClick(e){
+    // e.preventDefault();
+    let channelName = e.currentTarget.id;
+    console.log(channelName);
+    this.setState({
+      streamer: channelName
+    })
   }
 
   listTopStreamers(){
     return (
       this.state.streams.map(stream=>
-        <div className="streamer">
-          <div className="name">{stream.channel.name}</div>
-          <div className="viewers">{stream.viewers}</div>
-        </div>
+        <li key={stream["_id"]}
+            onClick={this.handleStreamerSelectionClick.bind(this)}
+            className="streamer"
+            id={stream.channel.name}>
+          <a href="">
+            <div className="name">{stream.channel.name}</div>
+            <div className="viewers">{stream.viewers}</div>
+          </a>
+        </li>
       )
     )
   }
@@ -46,7 +61,7 @@ export class TwitchBlock extends Component {
     else{
       return (
           <iframe
-              src={`http://player.twitch.tv/?channel=${this.state.streams[0].channel.name}`}
+              src={`http://player.twitch.tv/?channel=${this.state.streamer}&muted=true`}
               width="100%"
               height="100%"
               scrolling="no">
@@ -55,16 +70,18 @@ export class TwitchBlock extends Component {
     }
   }
 
+
+
   render() {
     return (
         <li className={'home__block streams block-width-2'}>
-          <Link to={'/streamerzy'}>
-            <div className="header">Aktualni Nadawcy</div>
+          <Link to={'/twitch'}>
+            <div className="header">Current Top HS Broadcasters</div>
           </Link>
           <div className="streams-body">
-            <div className="live-broadcasters">
+            <ul className="live-broadcasters">
               {this.listTopStreamers()}
-            </div>
+            </ul>
 
 
             <div className="iframe-container">
@@ -76,3 +93,8 @@ export class TwitchBlock extends Component {
     );
   }
 }
+
+TwitchBlock.propTypes = {
+  streams: React.PropTypes.array,
+  streamer: React.PropTypes.string
+};
