5a81ff108486a5de8e427ed4729f8061d40f217a
xNehel <eskrzy@gmail.com>
2017-03-10 15:48:46 +0100

    Changed create deck block in homepage to be stateless component

--- src/containers/pages/home/create-deck/create-deck.js
+++ src/containers/pages/home/create-deck/create-deck.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router'
-export class CreateDeckBlock extends Component {
-  render() {
-    return (
-        <li className={'home__block create-deck block-width-1'}>
-          <Link to={'/stworz-talie-kart'}>
-            <div className="header">Stwórz talię</div>
-            <div className="icon">
-              <span className="hs-icon icon-create-deck"></span>
-            </div>
-          </Link>
+const CreateDeckBlock = () => {
+  return (
+      <li className={'home__block create-deck block-width-1'}>
+        <Link to={'/create-deck'}>
+          <div className="header">Create deck</div>
+          <div className="icon">
+            <span className="hs-icon icon-create-deck"></span>
+          </div>
+        </Link>
 
-        </li>
-    );
-  }
-}
+      </li>
+  );
+};
+
+export default CreateDeckBlock;
