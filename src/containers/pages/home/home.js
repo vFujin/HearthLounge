f48a3c2e66b604760222d8d716737713c433f073
xNehel <eskrzy@gmail.com>
2017-03-10 15:45:29 +0100

    Changed tournaments block in homepage to be stateless component

--- src/containers/pages/home/home.js
+++ src/containers/pages/home/home.js
@@ -3,7 +3,7 @@ import DecksBlock from './decks/decks';
 import ArenaPickerBlock from './arena-picker/arena-picker';
 import CardsBlock from './cards/cards';
 import ExtensionsBlock from './extensions/extensions';
-import { TournamentsBlock } from './tournament/tournaments';
+import TournamentsBlock from './tournament/tournaments';
 import { CreateDeckBlock } from './create-deck/create-deck';
 import { TwitchBlock } from './twitch/twitch';
 import { ForumBlock } from './forum/forum';
--- src/containers/pages/home/tournament/tournaments.js
+++ src/containers/pages/home/tournament/tournaments.js
@@ -1,19 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router';
-export class TournamentsBlock extends Component {
-  render() {
-    return (
-        <li className={'home__block tournaments block-width-1'}>
-          <Link to={'/turnieje'}>
-            <div className="header">Turnieje</div>
-          </Link>
-          <iframe src="https://www.google.com/calendar/embed?src=kvaverirkumds90dnen1jlmmq0dcvgom%40import.calendar.google.com&ctz=America/New_York"
-                  style={{border: 'none', margin: '10px'}}
-                  width="95%"
-                  height="82%"
-                  frameBorder="0"
-                  scrolling="no"></iframe>
-        </li>
-    );
-  }
-}
+
+const TournamentsBlock = () => {
+  let calendar = "https://www.google.com/calendar/embed?src=kvaverirkumds90dnen1jlmmq0dcvgom%40import.calendar.google.com&ctz=America/New_York";
+  return (
+      <li className={'home__block tournaments block-width-1'}>
+        <Link to={'/tournaments'}>
+          <div className="header">Tournaments</div>
+        </Link>
+        <iframe
+            src={calendar}
+            style={{border: 'none', margin: '10px'}}
+            width="95%"
+            height="82%"
+            frameBorder="0"
+            scrolling="no"></iframe>
+      </li>
+  );
+};
+
+export default TournamentsBlock;
