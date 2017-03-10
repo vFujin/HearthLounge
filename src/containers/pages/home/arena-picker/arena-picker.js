1de26e2188a9fed1f2677bab22b8b9035154cc4d
xNehel <eskrzy@gmail.com>
2017-03-10 15:19:12 +0100

    Changed cards block in homepage to be stateless component

--- src/containers/pages/home/arena-picker/arena-picker.js
+++ src/containers/pages/home/arena-picker/arena-picker.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-const ArenaPickerBlock = () =>{
-    return (
-        <li className={'home__block arena-picker block-width-1'}>
-          <Link to={'/arena-picker'}>
-            <div className="header">Arena Picker</div>
-          </Link>
-          (filmik)
-        </li>
-    );
+const ArenaPickerBlock = () => {
+  return (
+      <li className={'home__block arena-picker block-width-1'}>
+        <Link to={'/arena-picker'}>
+          <div className="header">Arena Picker</div>
+        </Link>
+        (filmik)
+      </li>
+  );
 };
 
 export default ArenaPickerBlock;
--- src/containers/pages/home/cards/cards.js
+++ src/containers/pages/home/cards/cards.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router'
-export class CardsBlock extends Component {
-  render() {
-    return (
-        <li className={'home__block cards block-width-1'}>
-          <Link to={'/karty'}>
-            <div className="header">Karty</div>
-            <div className="icon">
-              <span className="hs-icon icon-card"></span>
-            </div>
-          </Link>
+const CardsBlock = () => {
+  return (
+      <li className={'home__block cards block-width-1'}>
+        <Link to={'/cards'}>
+          <div className="header">Cards</div>
+          <div className="icon">
+            <span className="hs-icon icon-card"></span>
+          </div>
+        </Link>
+      </li>
+  );
+};
 
-        </li>
-    );
-  }
-}
+export default CardsBlock;
