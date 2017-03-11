355eba366815b3798c637410d2db98a6da976be4
xNehel <eskrzy@gmail.com>
2017-03-11 15:29:21 +0100

    Added Loader to sidebar items

--- src/containers/pages/streams/sidebar.js
+++ src/containers/pages/streams/sidebar.js
@@ -1,33 +1,45 @@
 import React from 'react';
 import {Link} from 'react-router';
+import Loader from '../../shared-assets/loader';
 
 const Sidebar = props => {
+
+  const loadPreviews = () =>{
+    if(props.streams < 1){
+      return <Loader/>;
+    }
+    else{
+      return (
+        props.streams.map(stream =>
+          <li className="preview" key={stream['_id']}>
+            <Link to="{stream_name}" >
+              <div className="preview-info">
+                <div className="preview-topbar">
+                  <div className="preview-name">{stream.channel.display_name}</div>
+                  <div className="preview-viewers">
+                    <span className="hs-icon icon-login"></span>
+                    <p>{stream.viewers}</p>
+                  </div>
+                </div>
+                <div className="preview-bottombar">
+                  <div className="preview-status">
+                    <p>{stream.channel.status}</p>
+                  </div>
+                </div>
+              </div>
+              <img src={stream.preview.large} alt={`${stream.channel.display_name}'s preview`}/>
+            </Link>
+          </li>
+        )
+      )
+    }
+  };
 
     return (
         <div className="sidebar">
           <h3 className="filter-header">Streamers</h3>
           <ul>
-            {props.streams.map(stream =>
-              <li className="preview" key={stream['_id']}>
-                <Link to="{stream_name}" >
-                  <div className="preview-info">
-                    <div className="preview-topbar">
-                      <div className="preview-name">{stream.channel.display_name}</div>
-                      <div className="preview-viewers">
-                        <span className="hs-icon icon-login"></span>
-                        <p>{stream.viewers}</p>
-                      </div>
-                    </div>
-                    <div className="preview-bottombar">
-                      <div className="preview-status">
-                        <p>{stream.channel.status}</p>
-                      </div>
-                    </div>
-                  </div>
-                  <img src={stream.preview.large} alt={`${stream.channel.display_name}'s preview`}/>
-                </Link>
-              </li>
-            )}
+            {loadPreviews()}
           </ul>
         </div>
     );
