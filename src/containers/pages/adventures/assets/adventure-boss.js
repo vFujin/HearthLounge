30c8947dc5cc0e126eacbeb3a1f2854fab001ca6
xNehel <eskrzy@gmail.com>
2017-03-12 16:05:09 +0100

    Fixed adventure boss routing

--- src/containers/pages/adventures/assets/adventure-boss.js
+++ src/containers/pages/adventures/assets/adventure-boss.js
@@ -5,22 +5,101 @@ import NotFound from '../../../shared-assets/not-found';
 
 export class AdventureBoss extends Component {
 
-  validateUrl(){
-    const {adventure, details, boss} = this.props;
-    let wing_path = adventure_details.filter(x=>x.adventure === adventure).map(x=>x.bosses.details)[0].map(x=>x.url).includes(details);
-    let wing_boss = adventure_details.filter(x=>x.adventure === adventure).map(x=>x.bosses.details)[0].filter(x=>x.url === details)[0].bosses.map(x=>x.url).includes(boss);
-
-    let boss_name = this.getBossDetailsFromUrl('boss', details, boss);
-    let wing = this.getBossDetailsFromUrl('wing', details, boss);
-    let reward = this.getBossDetailsFromUrl('reward', details, boss);
-
-    if(wing_path !== true && wing_boss === undefined){
-      return <NotFound page="boss" url="bosses"/>;
+  blocks(index, adventure, wing, wingUrl, bossName, bossUrl, bossReward) {
+    switch (index) {
+        /**
+         * Case:
+         * 0 - Overview
+         * 1 - Strategy
+         * 2 - Rewards
+         * 3 - List of rest bosses in a wing
+         */
+      case 0:
+        return (
+            <div>
+              <img src={`https://raw.githubusercontent.com/xNehel/HearthLounge/master/src/images/adventures/${adventure}/${wingUrl}/${bossUrl}.jpg`}
+                   alt={bossName}/>
+              <p>{bossName} is a (#) boss in {wing}</p>
+            </div>
+        );
+      case 1:
+        return (
+            <div>
+              <p>Strategy list</p>
+            </div>
+        );
+      case 2:
+        return (
+            <div>
+              <p>{bossReward}</p>
+            </div>
+        );
+      case 3:
+        return (
+            <div>
+              <p>List of rest bosses in a wing</p>
+            </div>
+        );
+      default:
+        return null;
     }
-    else{
+  }
+
+  firstRow(bossName, wingName, bossReward) {
+    let adventure = this.props.adventure;
+    let wingUrl = this.props.details;
+    let bossUrl = this.props.boss;
+    return boss_details.slice(0, 4).map((boss, index) =>
+        <li className="block" key={index}>
+          <div className="block-content">
+            <p className="boss-details-nav-el">{boss.name}</p>
+            {this.blocks(index, adventure, wingName, wingUrl, bossName, bossUrl, bossReward)}
+          </div>
+        </li>)
+  }
+
+  secondRow() {
+    return boss_details.slice(4, 5).map((boss, index) =>
+        <li className="block" key={index}>
+          <div className="block-content">
+            <p className="boss-details-nav-el">{boss.boss}</p>
+            <div className="top-boss-decks">
+              <DeckSnippet />
+              <DeckSnippet />
+              <DeckSnippet />
+            </div>
+          </div>
+        </li>
+    )
+  }
+
+  getBossDetailsFromUrl(detail, wing, boss) {
+    let detailsArr = adventure_details.map(x => x.bosses.details.filter(x => x.url === wing)[0]).filter(x => x)[0],
+        bossArr    = detailsArr.bosses.filter(x => x.url === boss)[0];
+
+    switch (detail) {
+      case 'boss':   return bossArr.boss;
+      case 'wing':   return detailsArr.wing_title;
+      case 'reward': return bossArr.reward;
+      default: return null;
+    }
+  }
+
+  validateUrl() {
+    const {adventure, details, boss} = this.props;
+    let wing_boss = adventure_details.filter(x => x.adventure === adventure).map(x => x.bosses.details)[0].filter(x => x.url === details)[0].bosses.map(x => x.url).includes(boss);
+
+    if (wing_boss !== true) {
+      return <NotFound page="boss" url="adventures/curse-of-naxxramas/bosses"/>;
+    }
+    else {
+      let boss_name = this.getBossDetailsFromUrl('boss', details, boss),
+          wing = this.getBossDetailsFromUrl('wing', details, boss),
+          reward = this.getBossDetailsFromUrl('reward', details, boss);
+
       return (
           <div className={`boss inner-container ${this.props.details && 'active'}-view`}>
-            {adventure_details.slice(0,1).map((adventure, i)=>
+            {adventure_details.slice(0, 1).map((adventure, i) =>
                 <div key={i} className="boss-guide-header">
                   <p className="boss-details-nav-el">{boss_name}</p>
                   <ul>
@@ -32,91 +111,7 @@ export class AdventureBoss extends Component {
           </div>
       );
     }
-
   }
-
-
-    blocks(index, adventure, wing, wingUrl, bossName, bossUrl, bossReward){
-
-      switch(index){
-        /**
-           * Case:
-           * 0 - Overview
-           * 1 - Strategy
-           * 2 - Rewards
-           * 3 - List of rest bosses in a wing
-           */
-        case 0:
-          return (
-            <div>
-              <img src={`https://raw.githubusercontent.com/xNehel/HearthLounge/master/src/images/adventures/${adventure}/${wingUrl}/${bossUrl}.jpg`} alt={bossName}/>
-              <p>{bossName} is a (#) boss in {wing}</p>
-            </div>
-        );
-        case 1:
-          return (
-            <div>
-              <p>Strategy list</p>
-            </div>
-          );
-        case 2:
-          return (
-            <div>
-              <p>{bossReward}</p>
-            </div>
-          );
-        case 3:
-          return (
-            <div>
-              <p>List of rest bosses in a wing</p>
-            </div>
-          );
-        default: return null;
-      }
-    }
-
-    firstRow(bossName, wingName, bossReward){
-      let adventure = this.props.adventure;
-      let wingUrl = this.props.details;
-      let bossUrl = this.props.boss;
-     return boss_details.slice(0,4).map((boss, index)=>
-          <li className="block" key={index}>
-            <div className="block-content">
-              <p className="boss-details-nav-el">{boss.name}</p>
-              {this.blocks(index, adventure, wingName, wingUrl, bossName, bossUrl, bossReward)}
-            </div>
-          </li>)
-    }
-
-    secondRow(){
-      return boss_details.slice(4,5).map((boss, index)=>
-          <li className="block" key={index}>
-            <div className="block-content">
-              <p className="boss-details-nav-el">{boss.boss}</p>
-              <div className="top-boss-decks">
-                <DeckSnippet />
-                <DeckSnippet />
-                <DeckSnippet />
-              </div>
-            </div>
-          </li>
-      )
-    }
-
-    getBossDetailsFromUrl(detail, wing, boss){
-      let detailsArr = adventure_details.map(x=>x.bosses.details.filter(x=>x.url===wing)[0]).filter(x=>x)[0];
-      let bossArr = detailsArr.bosses.filter(x=>x.url===boss)[0];
-      switch(detail){
-        case 'boss':
-          return bossArr.boss;
-        case 'wing':
-          return detailsArr.wing_title;
-        case 'reward':
-          return bossArr.reward;
-        default:
-          return null;
-      }
-    }
 
   render() {
     return this.validateUrl();
--- src/containers/pages/adventures/right-container/adventure.js
+++ src/containers/pages/adventures/right-container/adventure.js
@@ -6,17 +6,16 @@ import NotFound from '../../../shared-assets/not-found';
 export class Adventure extends Component{
 
   validateUrl(){
-    let path = this.props.location.pathname.split("/")[2];
-    let adventures = navItems.filter(x=>x.name === 'adventures').map(x=>x.submenu)[0].map(x=>x.url).includes(path);
-
-    let adventureUrl = this.props.params.adventure;
-    let detailsUrl   = this.props.params.details;
-    let bossUrl      = this.props.params.boss;
+    let path         = this.props.location.pathname.split("/")[2],
+        adventures   = navItems.filter(x=>x.name === 'adventures').map(x=>x.submenu)[0].map(x=>x.url).includes(path),
+        adventureUrl = this.props.params.adventure,
+        detailsUrl   = this.props.params.details,
+        bossUrl      = this.props.params.boss;
 
     if(adventures !== true){
       return <NotFound page="adventure" url="adventures"/>;
     }
-    else{
+    else {
       return(
         <div className="content">
           <Topbar adventure={adventureUrl}
@@ -27,7 +26,6 @@ export class Adventure extends Component{
       )
     }
   }
-
 
   render() {
     return this.validateUrl()
