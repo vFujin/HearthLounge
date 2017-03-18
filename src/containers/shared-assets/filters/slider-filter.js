75f406a38f16e1da401064e7572d107ee92809b7
xNehel <eskrzy@gmail.com>
2017-03-19 00:56:25 +0100

    Added slider filter styles

--- src/containers/shared-assets/filters/slider-filter.js
+++ src/containers/shared-assets/filters/slider-filter.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import Slider from 'antd/lib/slider';
-import {addQuery, removeQuery} from '../../../utils/utils-router';
+import {addQuery} from '../../../utils/utils-router';
 import 'antd/lib/slider/style/css';
 
 const SliderFilter = props => {
   const {filter, query} = props;
 
   const queries = attr =>{
+    console.log(attr);
     return Object.assign({}, query, {[filter]: attr});
   };
-
-  function onChange(value) {
-    console.log('onChange: ', value);
-  }
 
   const marks = {
     0: 0,
@@ -27,7 +24,7 @@ const SliderFilter = props => {
   return (
       <div className="input-filter-wrapper">
         <h4>{filter}</h4>
-          <Slider range marks={marks} defaultValue={[20, 50]} max={50} onChange={onChange}/>
+          <Slider range marks={marks} defaultValue={[0, 30]} max={50} onChange={(e)=>addQuery(queries(e))}/>
       </div>
   );
 };
