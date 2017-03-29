ef149f7ac529ab8c92710ffc673762aac97c7c32
xNehel <eskrzy@gmail.com>
2017-03-29 16:53:13 +0200

    Added gradient background based on rarity of the card for tr

--- src/containers/pages/create-deck/picked-class/left-container/sidebar/choosen-cards.js
+++ src/containers/pages/create-deck/picked-class/left-container/sidebar/choosen-cards.js
@@ -2,19 +2,10 @@ import React from 'react';
 import _ from 'lodash';
 const ChoosenCards = props => {
   const {deck, countCards} = props;
-console.log(props.params);
-  const gradient = params => {
-    return {
-      'background': '-moz-linear-gradient(left, rgba(0,0,0,0.65) 0%, rgba(0,0,0,0) 100%)',
-      'background': '-webkit-linear-gradient(left, rgba(0,0,0,0.65) 0%,rgba(0,0,0,0) 100%)',
-      'background': 'linear-gradient(to right, rgba(0,0,0,0.65) 0%,rgba(0,0,0,0) 100%)',
-      'filter': "progid:DXImageTransform.Microsoft.gradient( startColorstr='#a6000000', endColorstr='#00000000',GradientType=1)",
-    }
-  };
 
   const listCards = () => {
     return _.uniqBy(_.sortBy(deck, ['cost', 'name'])).map((card, i) =>
-        <tr key={i} className={gradient(props.params)}>
+        <tr key={i} className={`${_.toLower(card.rarity)} gradient`}>
           <td><span className={`hs-icon icon-${_.kebabCase(card.cardSet)}`}></span></td>
           <td>{card.name}</td>
           <td>{countCards(card)}</td>
