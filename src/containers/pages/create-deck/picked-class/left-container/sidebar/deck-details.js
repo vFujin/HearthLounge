1cdb3b861822c5dd7d6acf98e4668b53bca4a2e9
xNehel <eskrzy@gmail.com>
2017-03-31 20:45:57 +0200

    Fixed navbar icons with submenus in chrome

--- src/containers/pages/create-deck/picked-class/left-container/sidebar/deck-details.js
+++ src/containers/pages/create-deck/picked-class/left-container/sidebar/deck-details.js
@@ -4,7 +4,7 @@ import _ from 'lodash';
 const DeckDetails = ({deck, deckDetails, mechanics}) => {
   let deckMechanics = [].concat.apply([], _.map(deck, (value)=>value.hasOwnProperty('mechanics') ? value.mechanics : null));
   let countMechanics = _.countBy(deckMechanics, 'name');
-
+  console.log(_.sortBy(countMechanics, Object.values(countMechanics)))
   const listMechanics = () =>{
     return mechanics.map(mechanic=>
         <tr key={mechanic}>
