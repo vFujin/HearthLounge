5bf74c96942d03921a834c98039b1a862261259e
xNehel <eskrzy@gmail.com>
2017-04-09 15:35:39 +0200

    Minor styling

--- src/containers/pages/dashboard/dashboard.js
+++ src/containers/pages/dashboard/dashboard.js
@@ -4,11 +4,25 @@ import {Sidebar} from './left-container/sidebar';
 import {Topbar} from './right-container/topbar'
 
 export class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: null
+    }
+  }
   componentWillMount(){
     if(this.props.authed === false){
       browserHistory.push('/sign-in');
     }
   }
+  componentWillReceiveProps(nextProps){
+    console.log(nextProps)
+    this.setState({
+      user: nextProps.user
+    })
+  }
+
 
   render(){
 
@@ -16,7 +30,7 @@ export class Dashboard extends Component {
         <div className="container__page container__page--twoSided dashboard">
           <div className="container__page--inner container__page--left">
             {this.props.authed}
-            <Sidebar/>
+            <Sidebar user={this.props.user}/>
           </div>
           <div className="container__page--inner container__page--right">
             <Topbar/>
--- src/containers/pages/dashboard/left-container/sidebar.js
+++ src/containers/pages/dashboard/left-container/sidebar.js
@@ -2,81 +2,76 @@ import React, { Component } from 'react';
 // import Select from 'react-select';
 
 export class Sidebar extends Component {
+
   render() {
+    console.log(this.props);
     return (
         <div className="sidebar">
-          <h3 className="sidebar__header">Profile</h3>
+          <h3 className="sidebar__header">Profile <button className="btn-pearl">Edit</button></h3>
 
           <ul className="sidebar__body">
             <li className="about">
               <div className="avatar"><img src="http://lorempixel.com/100/100/cats/" alt="cat"/></div>
-              <div className="username">xx_Joe_xx</div>
-              <label htmlFor="email">
-                <input id="email" type="text" value="joe@placeholder.com"/>
-              </label>
+              <div className="username">Joe</div>
             </li>
 
             <li className="details">
-              <h3><span>Details</span></h3>
-              <label htmlFor="firstname">
-                <p>First name:</p>
-                <input id="firstname" type="text" value="Joe"/>
-              </label>
-              <label htmlFor="lastname">
-                <p>Last name:</p>
-                <input id="lastname" type="text" value="Smith"/>
-              </label>
-              <label htmlFor="gender">
-                <p>Gender:</p>
-                {/*<Select*/}
-                    {/*placeholder="Mechaniki..."*/}
-                    {/*value={this.props.mechanics}*/}
-                    {/*options={mechanics}*/}
-                    {/*onChange={this.props.handleInputFilter.bind(this, 'mechanics')}*/}
-                {/*/>*/}
-              </label>
-              <div className="country">'Murica</div>
-              <div className="signature">random text</div>
+              <h3>Details</h3>
+              <div>
+                <label htmlFor="email">
+                  <p>E-mail</p>
+                  <input id="email" type="text" value="joe@placeholder.com"/>
+                </label>
+                {/*<label htmlfor="signature">Signature</label>*/}
+                {/*<textarea id="signature" className="signature">random text</textarea>*/}
+              </div>
             </li>
 
             <li className="hearthstone">
-              <h3><span>Hearthstone</span></h3>
-              <label htmlFor="battletag">
-                <span className="hs-icon icon-battlenet"></span>
-                <input id="battletag" type="text" value="Placeholder#0000"/>
-              </label>
-              <label htmlFor="favourite-class">
-                <p>Favourite class:</p>
-                <span className="hs-icon icon-warlock"></span>
-                <input id="favourite-class" type="text" value="Warlock"/>
-              </label>
-              <label htmlFor="region">
-                <p>Region:</p>
-                <input id="region" type="text" value="EU"/>
-              </label>
+              <h3>Hearthstone</h3>
+              <div>
+                <label htmlFor="battletag">
+                  <span className="hs-icon icon-battlenet"></span>
+                  <input id="battletag" type="text" value="Placeholder#0000"/>
+                </label>
+                <label htmlFor="favourite-class">
+                  <p>Favourite class:</p>
+                  <span className="hs-icon icon-warlock"></span>
+                  {/*<input id="favourite-class" type="text" value="Warlock"/>*/}
+                </label>
+                <label htmlFor="region">
+                  <p>Region:</p>
+                  <Select multiple={false}
+                          style={{width: "100%"}}
+                          placeholder="EU #1">
+                    {options}
+                  </Select>
+                </label>
+              </div>
             </li>
 
             <li className="social-media">
-              <h3><span>Social Media</span></h3>
-              <label htmlFor="facebook">
-                <span className="hs-icon icon-facebook"></span>
-                <input id="facebook" type="text" value="Placeholder"/>
-              </label>
-              <label htmlFor="twitter">
-                <span className="hs-icon icon-twitter"></span>
-                <input id="twitter" type="text" value="Placeholder"/>
-              </label>
-              <label htmlFor="twitch">
-                <span className="hs-icon icon-twitch"></span>
-                <input id="twitch" type="text" value="Placeholder"/>
-              </label>
-              <label htmlFor="youtube">
-                <span className="hs-icon icon-youtube"></span>
-                <input id="youtube" type="text" value="Placeholder"/>
-              </label>
+              <h3>Social Media</h3>
+              <div>
+                <label htmlFor="facebook">
+                  <span className="hs-icon icon-facebook"></span>
+                  <input id="facebook" type="text" value="Placeholder"/>
+                </label>
+                <label htmlFor="twitter">
+                  <span className="hs-icon icon-twitter"></span>
+                  <input id="twitter" type="text" value="Placeholder"/>
+                </label>
+                <label htmlFor="twitch">
+                  <span className="hs-icon icon-twitch"></span>
+                  <input id="twitch" type="text" value="Placeholder"/>
+                </label>
+                <label htmlFor="youtube">
+                  <span className="hs-icon icon-youtube"></span>
+                  <input id="youtube" type="text" value="Placeholder"/>
+                </label>
+              </div>
             </li>
           </ul>
-          <button>Edit</button>
         </div>
     );
   }
