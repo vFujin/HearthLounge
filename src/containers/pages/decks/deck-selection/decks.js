050a97750fa36ae1f502e3d4044b262c41152b9f
xNehel <eskrzy@gmail.com>
2017-05-13 20:40:37 +0200

    Docs - Decks function description - update

--- src/containers/pages/decks/deck-selection/decks.js
+++ src/containers/pages/decks/deck-selection/decks.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 /**
  * Function representing Decks page container, that is DeckList page and DeckDetails page
  *
- * @param {object} children -
+ * @param {object} children - Either DeckList or DeckDetails page, based on given URL
  * @param {object[]} cards - Array of all obtainable by player cards
  * @returns {*} - Returns DeckList and DeckDetails pages
  * @constructor
