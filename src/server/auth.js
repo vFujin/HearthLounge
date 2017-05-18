afb8633bb539bb93d409b3fedf7de1a85906f827
xNehel <eskrzy@gmail.com>
2017-05-18 23:55:57 +0200

    Docs - docs update

--- src/server/auth.js
+++ src/server/auth.js
@@ -1,12 +1,16 @@
 import {ref, firebaseAuth} from '../keys';
 import {browserHistory} from 'react-router';
 export function createUser(email, pass, username, callback){
-
   let promise = firebaseAuth().createUserWithEmailAndPassword(email, pass).then(user=>saveUser(user, username));
   promise.catch(e => callback(e.message));
   return promise;
 }
 
+/**
+ * Function representing logout
+ * @param {func} e - event
+ * @returns {any|firebase.Thenable<any>}
+ */
 export function logout(e){
   e.preventDefault();
   return firebaseAuth().signOut()
@@ -14,8 +18,14 @@ export function logout(e){
       .catch(e=>console.log(e.message));
 }
 
+/**
+ * Function representing Sign In.
+ * @param {string} email - user e-mail
+ * @param {string} pass - user password
+ * @param {func} cb - callback
+ * @returns {any|firebase.Thenable<any>}
+ */
 export function signIn(email, pass, cb){
-
   return firebaseAuth().signInWithEmailAndPassword(email, pass)
       .then(()=>browserHistory.push('/dashboard'))
       .catch(e=>{
@@ -28,6 +38,11 @@ export function signIn(email, pass, cb){
 //   return firebaseAuth().sendPasswordResetEmail(email);
 // }
 
+/**
+ * Function representing user saving to Firebase
+ * @param {object} user - user details
+ * @param {string} username - unique username
+ */
 function saveUser(user, username){
   return ref.child(`users/${user.uid}`)
       .set({
