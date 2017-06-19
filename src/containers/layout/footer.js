a91be02bab4853585e131130abc19cf8501e3cb2
xNehel <eskrzy@gmail.com>
2017-06-19 23:54:57 +0200

    Icons + added paypal / discord icons

--- src/containers/layout/footer.js
+++ src/containers/layout/footer.js
@@ -2,7 +2,13 @@ import React from 'react';
 const Footer = () =>{
 
     return (
-        <footer><p style={{'fontSize': '8px', color: 'white'}}>pre-alpha 0.1.0</p>copyright yada yada</footer>
+        <footer>
+            <p style={{'fontSize': '8px', color: 'white'}}>© 2017 Hearth Lounge Terms Privacy</p>
+            <span className="hs-icon icon-facebook"></span>
+            <span className="hs-icon icon-github"></span>
+            <span className="hs-icon icon-twitch"></span>
+            <span className="hs-icon icon-discord"></span>
+        </footer>
     );
 };
 
