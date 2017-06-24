d8eaef23e5e6ccaab9126a82a6ba710a35ca7680
Sebastian Krzyżanowski <eskrzy@gmail.com>
2017-06-24 13:28:16 +0200

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,7 @@
 import firebase from 'firebase';
-export const TwitchClientId = "";
-export const MashapeKey = '';
-export const FirebaseConfig = {
-};
+export const TwitchClientId = "<YOUR TWITCH CLIENT ID>"; //https://dev.twitch.tv/docs/v5/guides/using-the-twitch-api
+export const MashapeKey = '<YOUR MASHAPE/REDIS KEY';     //https://market.mashape.com/omgvamp/hearthstone
+export const FirebaseConfig = { <YOUR FIREBASE CONFIG OBJECT> }; //https://console.firebase.google.com/u/0/ 
 firebase.initializeApp(FirebaseConfig);
 export const refParent = (parent) => {
   return firebase.database().ref(parent);
