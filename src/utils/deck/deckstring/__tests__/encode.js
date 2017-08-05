e122f1e631d051e1c9439e5f055e4896f7c80c36
xNehel <eskrzy@gmail.com>
2017-08-05 15:53:42 +0200

    Tests + added tests for decoding deckstring

--- src/utils/deck/deckstring/__tests__/encode.js
+++ src/utils/deck/deckstring/__tests__/encode.js
@@ -14,15 +14,27 @@ describe('encodes deck to deckstring', ()=>{
     format: 2
   };
 
+  const test_deck_3 = {
+    cards: [[739, 2], [284, 2], [906, 1]],
+    heroes: [1066],
+    format: 2
+  };
+
+  const test_deck_4 = {
+    cards: [[739, 2], [284, 2], [906, 1]],
+    heroes: [274],
+    format: 1
+  };
+
 
   const testDecoding = (testingDeckName, format, deck, expectedValue) =>{
     test(`encodes ${format} test deck ${testingDeckName} to deckstring`, ()=>{
-      let eStringifiedValue = encodeDeckstring(deck);
-
-      expect(eStringifiedValue).toBe(expectedValue);
+      expect(encodeDeckstring(deck)).toBe(expectedValue);
     })
   };
 
   testDecoding(1, 'wild', test_deck_1, "AAEBAa0GAtkKigcC4wWcAgA=");
   testDecoding(2, 'standard', test_deck_2, "AAECAa0GAtkKigcC4wWcAgA=");
+  testDecoding(3, 'standard', test_deck_3, "AAECAaoIAYoHAuMFnAIA");
+  testDecoding(4, 'adventure', test_deck_4, "AAEBAZICAYoHAuMFnAIA");
 });
