32c5a0dd5f218cb1277124f7d9ed27d5b71fce44
Reden <reden@armstead.io>
2017-08-30 00:16:59 +0800

    idea

--- src/data/dashboard.js
+++ src/data/dashboard.js
@@ -1,3 +1,14 @@
+// might come handy in the future
+// if you are only going to export one variable
+// prefer default
+
+// export default {
+//  region: ['America', 'Asia', 'Europe'],
+//  classes: icon_filters.playerClass.map((e) => e.name)
+// }
+
+// also refrain from using libraries for 1 liner code :)
+
 import {icon_filters} from './filters';
 import _ from 'lodash';
 export const dashboard_data = {
