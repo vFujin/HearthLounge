cc9ddd80f984ce4ef09091d8bcf6a90ed9ff2cc4
xNehel <eskrzy@gmail.com>
2017-12-24 06:43:01 +0100

    Changed placeholder in extension selection view

--- src/containers/shared-assets/extensions/select-extension.js
+++ src/containers/shared-assets/extensions/select-extension.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Icon from "../../../components/icon";
 
 const SelectExtension = ({group}) => {
   return (
-      <div className="select-extension">
-        LEFT ARROW select {group}
+      <div className="select-extension hs-font">
+        <Icon name="select" />
+        <p>select {group}</p>
       </div>
   )
 };
