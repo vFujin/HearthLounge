d1c1fab131ae1d92d5499ba1b2516f97370a0ebc
xNehel <eskrzy@gmail.com>
2018-01-01 20:58:24 +0100

    Added actions tests to deck tools

---
+++ src/redux/deck/tools/__tests__/actions.test.js
@@ -0,0 +1,20 @@
+import * as actions from '../actions';
+import * as types from '../types';
+import lowerCase from 'lodash/lowerCase';
+
+describe('#deleteDeckActions', () =>{
+  const testActions = (action, type, key, state) =>{
+    test(`should create an action to ${lowerCase(type)}`, () =>{
+        const expectedAction = {
+          type: type,
+          [key]: state
+        };
+        expect(action(state)).toEqual(expectedAction);
+    })
+  };
+
+  testActions(actions.toggleDeckEditView, types.TOGGLE_DECK_EDIT_VIEW, 'payload', true);
+  testActions(actions.updateCommentText, types.UPDATE_COMMENT_TEXT, 'payload', "foo");
+  testActions(actions.toggleCommentBox, types.TOGGLE_COMMENT_BOX, 'payload', true);
+  testActions(actions.togglePreview, types.TOGGLE_PREVIEW, 'payload', true);
+});
