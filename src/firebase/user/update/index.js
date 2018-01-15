ed0a65c53b14abecdf7dede4d34d2f6c57e4dc32
xNehel <eskrzy@gmail.com>
2018-01-15 01:30:20 +0100

    Removed unused firebase storage function

--- src/firebase/user/update/index.js
+++ src/firebase/user/update/index.js
@@ -1,4 +1,3 @@
-export {default as updateAvatar} from './avatar';
 export {default as updateEmail} from './email';
 export {default as updateHearthstoneData} from './hearthstone';
 export {default as updateSocialMediaData} from './social-media';
