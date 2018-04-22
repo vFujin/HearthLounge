b1318b2a283a6a9e115d0b6bcbca7bbe77a9792a
xNehel <eskrzy@gmail.com>
2018-04-23 00:43:30 +0200

    couple mobile nav ui fixes

--- src/containers/layout/navbar/navbar-mobile.js
+++ src/containers/layout/navbar/navbar-mobile.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { connect } from "react-redux";
 import PropTypes from 'prop-types';
 import LogoItem from "./logo-item";
 import ItemsList from "./items-list/items-list";
@@ -7,32 +8,47 @@ import MobileMenuIcon from "./mobile-menu-icon/mobile-menu-icon";
 import './styles/navbar-styles.css';
 import './styles/navbar-mobile-styles.css';
 import './styles/navbar-media-queries.css';
+import {toggleMobileMenu} from "../../../redux/app/mobile-menu/actions";
 
-const NavbarMobile = ({handleSignOut, activeUser, playerClass, mobileMenuActive}) => {
-  return (
-    <nav className="nav__mobile">
-      <ul className="nav__mobile--header">
-        <LogoItem />
-        <MobileMenuIcon />
-      </ul>
-      {mobileMenuActive && (
-        <div>
-          <ul className="nav__mobile--pages">
-            <EntryNode handleSignOut={handleSignOut} activeUser={activeUser}/>
-            <li className="nav__mobile--scrollWrapper">
-              <ul>
-                <ItemsList playerClass={playerClass}/>
-              </ul>
-            </li>
-          </ul>
-        </div>
-      )}
-    </nav>
-  );
-};
+class NavbarMobile extends Component {
+
+  handleCloseMenuClick = () => {
+    this.props.closeMobileMenu(false);
+  };
+
+  render() {
+    const {handleSignOut, activeUser, playerClass, mobileMenuActive} = this.props;
+    return (
+      <nav className="nav__mobile">
+        <ul className="nav__mobile--header">
+          <LogoItem/>
+          <MobileMenuIcon/>
+        </ul>
+        {mobileMenuActive && (
+          <div onClick={this.handleCloseMenuClick}>
+            <ul className="nav__mobile--pages">
+              <EntryNode handleSignOut={handleSignOut} activeUser={activeUser}/>
+              <li className="nav__mobile--scrollWrapper">
+                <ul>
+                  <ItemsList playerClass={playerClass}/>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        )}
+      </nav>
+    );
+  };
+}
 
 NavbarMobile.propTypes = {
   activeUser: PropTypes.object
 };
 
-export default NavbarMobile;
+const mapDispatchToProps = dispatch => {
+  return {
+    closeMobileMenu: payload => dispatch(toggleMobileMenu(payload))
+  }
+};
+
+export default connect(null, mapDispatchToProps)(NavbarMobile);
