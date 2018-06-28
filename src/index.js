22fbf15a23fc9a1cfec9a7abed356d69e8f40692
xNehel <eskrzy@gmail.com>
2018-06-28 22:51:15 +0200

    reduced img width for first section

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
     rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
     applyMiddleware(sagaMiddleware),
 );
 
